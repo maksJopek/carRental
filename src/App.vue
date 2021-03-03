3db62abb2c4811667bd1e5cd77ed288f89d527f2
maksJopek <maks@jopek.eu>
2021-03-03 13:03:57 +0100

    Fontend of project

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<div id="app" v-if="gotData">
+		<CarItem v-for="car in avaibleCars" v-bind="car" :key="car.key" />
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { component } from "vue/types/umd";
+import CarItem from "./components/CarItem.vue";
+
+@Component({
+	components: {
+		CarItem,
+	},
+})
+export default class App extends Vue {
+	private gotData = false;
+	private avaibleCars = ((): Array<object> => {
+		this.gotData = true;
+		return [
+			{
+				name: "Opel Astra",
+				year: 2004,
+				price: 500,
+				photoUrl: "https://jopek.eu/opelAstra",
+				equipment: "klimatyzacja, czujnik parkowania",
+				key: 0,
+			},
+		];
+	})();
+}
+</script>
+
+<style>
+#app {
+	width: 70%;
+	margin: 5px auto;
+
+	/* font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px; */
+}
+#car {
+	border: 1px solid black;
+}
+</style>
---
+++ src/components/CarItem.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="car">
+		<div class="carName">
+			<p>
+				{{ name }}
+			</p>
+		</div>
+		<div class="carPhoto" :style="cssProps"></div>
+		<div class="carProps">
+			<ul>
+				<li>Rocznik: {{ year }}</li>
+				<li>Cena wynajmu na 1 dzień: {{ price }}zł</li>
+				<li>Wyposażenie: {{ equipment }}</li>
+			</ul>
+		</div>
+		<div class="carRent">
+			<button @click="carRent">Wypożycz ten samochód</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+// import { component } from "vue/types/umd";
+
+@Component
+export default class CarItem extends Vue {
+	@Prop({ required: true }) readonly name!: string;
+	@Prop({ required: true }) readonly year!: number;
+	@Prop({ required: true }) readonly price!: number;
+	@Prop({ required: true }) readonly photoUrl!: string;
+	@Prop({ required: true }) readonly equipment!: string;
+
+	get cssProps(): object {
+		return {
+			"background-image": `url(${this.photoUrl})`,
+		};
+	}
+	async carRent() {
+		return;
+	}
+}
+</script>
+
+<style scoped>
+.car {
+	width: 90%;
+	height: 8vh;
+	border: 1px solid green;
+}
+.car div {
+	height: 8vh;
+	float: left;
+}
+.carName {
+	width: 20%;
+	font-size: 25px;
+}
+.carPhoto {
+	width: 20%;
+	background-size: 100% 100%;
+}
+.carProps {
+	width: 40%;
+}
+.carProps ul {
+	columns: 2;
+	font-size: 15px;
+}
+.carRent {
+	display: flex;
+	align-items: center;
+	text-align: center;
+	width: 20%;
+}
+</style>
